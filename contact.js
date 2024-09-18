249eac1e869636e8113fd1015655060ea5fb5907
obedgodfrey <obedmarcelcomedian@gmail.com>
2024-09-18 01:57:09 +0100

    commit

---
+++ contact.js
@@ -0,0 +1,40 @@
+function Menu(e) {
+  const list = document.querySelector('ul')
+
+  if (e.name === 'menu') {
+    e.name = 'close'
+    list.classList.add('opacity-100')
+    list.classList.add('top-[130px]')
+  } else {
+    e.name = 'menu'
+    list.classList.remove('opacity-100')
+    list.classList.remove('top-[130px]')
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('contactForm')
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    // Client-side validation
+    const name = document.getElementById('name').value.trim()
+    const email = document.getElementById('email').value.trim()
+    const message = document.getElementById('message').value.trim()
+
+    if (name === '' || email === '' || message === '') {
+      alert('Please fill out all fields.')
+      return
+    }
+
+    // Submit form if validation passes
+    this.submit()
+  })
+})
+//
+
+const click = document.getElementById('click')
+click.addEventListener('click', () => {
+  document.location.pathname = 'about.html'
+})
